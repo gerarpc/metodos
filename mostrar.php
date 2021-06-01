fa1329b68dfb257a278388fc0d0c9eb7d0461890
gerarpc <gerarpc@gmail.com>
2021-06-01 18:22:56 -0500

    Clase de metodos POST y GET

---
+++ mostrar.php
@@ -0,0 +1,12 @@
+<?php
+    if(isset($_POST['name']) && isset($_POST['lastname']) && isset($_POST['email'])){
+        if ($_POST['name'] !== "" && $_POST['lastname'] !== "" && $_POST['email'] !== "") {
+        $value = 1;
+        }else {
+        $value =0;
+        }
+  
+    }
+    header("Location:./index.php?confirm=".$value)
+
+?>
